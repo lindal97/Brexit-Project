99ba586ee07c4d12bdbc7049e30e176829124644
lindal97 <44580938+lindal97@users.noreply.github.com>
2018-11-09 16:10:53 +0000

    Add files via upload

--- tweepy_streamer_Brexit_aws.py
+++ tweepy_streamer_Brexit_aws.py
@@ -13,7 +13,7 @@ Set:	yourtagone and yourtagtwo for firstthing
 	api keys
 Last modified: Nov 2018
 
-an updated version to write directly to AWS RDS repository, running on AWS lambda
+an updated version to write directly to AWS RDS repository, running on AWS EC2
 """
 import tweepy
 from tweepy import OAuthHandler
@@ -56,14 +56,14 @@ c.execute('''CREATE TABLE if NOT EXISTS users
 
 c.execute('''CREATE TABLE if NOT EXISTS keyword1
              (id CHAR(32) NOT NULL, user_id CHAR(24), user_screen_name CHAR(32), username CHAR(32),
-              created_at CHAR(100), fulltext VARCHAR, users_mentioned VARCHAR, url VARCHAR,
-              url_title VARCHAR, in_reply_to_userid CHAR(24), in_reply_to_userscreename CHAR(32)
+              created_at CHAR(100), fulltext VARCHAR, users_mentioned VARCHAR,
+              in_reply_to_userid CHAR(24), in_reply_to_userscreename CHAR(32)
               retweet_uid CHAR(24), retweet_id CHAR(32), retweet VARCHAR, PRIMARY KEY(id))''')
 
 c.execute('''CREATE TABLE if NOT EXISTS keyword2
              (id CHAR(32) NOT NULL, user_id CHAR(24), user_screen_name CHAR(32), username CHAR(32),
-              created_at CHAR(100), fulltext VARCHAR, users_mentioned VARCHAR, url VARCHAR,
-              url_title VARCHAR, in_reply_to_userid CHAR(24), in_reply_to_userscreename CHAR(32)
+              created_at CHAR(100), fulltext VARCHAR, users_mentioned VARCHAR,
+              in_reply_to_userid CHAR(24), in_reply_to_userscreename CHAR(32)
               retweet_uid CHAR(24), retweet_id CHAR(32), retweet VARCHAR,PRIMARY KEY(id))''')
 
 class MyListener(StreamListener):
@@ -90,50 +90,47 @@ class MyListener(StreamListener):
             retweet_id=''
             retweet_uid=''
             retweet=''
-            url=''
-            url_title=''
+            text=status.text
             user_info = (status.user.id_str, status.user.screen_name, status.user.name, 
                          status.user.description, status.user.location,status.user.followers_count,
                        status.user.friends_count,status.user.created_at)
             uid=status.user.id_str
-            c.execute('''SELECT * FROM users WHERE user_id=uid ''')
+            c.execute('''SELECT * FROM users WHERE user_id LIKE ''' + uid)
             re=c.fetchall()
-            if len(re)>0:
+            if len(re)==0:
                 c.execute(''' INSERT INTO users
                       (user_id, user_screen_name, username,user_description, 
                       user_location, user_followers, user_friends, user_created_at)
                       VALUES (%s,%s,%s,%s,%s,%s,%s,%s))''', user_info)
-            twitter = (status.id_str, status.user.id_str, status.user.screen_name, status.user.name, 
-                       status.created_at, status.text, users_mentioned, url, url_title,
-                       status.in_reply_to_user_id_str, status.in_reply_to_screen_name,
-                       retweet_id, retweet_uid,retweet)
-            if status.entities.user_mentions != []:
-                for i in range(len(status.entities.user_mentions)):
-                    user=status.entities.user_mentions[i].id_str
-                    users_mentioned.append += user+', '
+            
             if status.truncated is True:
-                try:
-                    twitter[5]=status.extended_tweet.full_text
-                except:
-                    pass
-            if status.retweeted_status is not None:
+                text=status.extended_tweet['full_text']
+            if hasattr(status, 'retweeted_status'):
                 retweet_id=status.retweeted_status.id_str
                 retweet_uid=status.retweeted_status.user.id_str
                 retweet=status.retweeted_status.text
-            if status.entities.urls.unwound is not None:
-                url=status.entities.urls.unwound.url
-                url_title=status.entities.urls.unwound.title
+            if hasattr(status, 'quote_status'):
+                retweet_id=status.quoted_status.id_str
+                retweet_uid=status.quoted_status.user.id_str
+                retweet=status.quoted_status.text
+            twitter = (status.id_str, status.user.id_str, status.user.screen_name, status.user.name, 
+                       status.created_at, text, 
+                       status.in_reply_to_user_id_str, status.in_reply_to_screen_name,
+                       retweet_id, retweet_uid,retweet)
             order='insert into '+ table + '''(id, user_id, 
                 user_screen_name, username, created_at, fulltext, 
-                users_mentioned, url, url_title, in_reply_to_userid, 
+                users_mentioned,  in_reply_to_userid, 
                 in_reply_to_userscreename, retweet_uid, retweet_id, retweet)
-                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
+            twitter = (status.id_str, status.user.id_str, status.user.screen_name, status.user.name, 
+                   status.created_at, status.text, users_mentioned, 
+                   status.in_reply_to_user_id_str, status.in_reply_to_screen_name,
+                   retweet_id, retweet_uid,retweet)
             c.execute(order, twitter)
             conn.commit()
         timestamp_previous=time.strftime("%Y%m%d_%H")
     def on_error(self, status):
         print(status)
-        conn.rollback()
         
 twitter_stream = Stream(auth, MyListener())
 while True:
