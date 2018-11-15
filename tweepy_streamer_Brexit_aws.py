269f6a472ca66e8fcee335fed301a7207c4b1640
lindal97 <44580938+lindal97@users.noreply.github.com>
2018-11-15 20:42:29 +0000

    Update tweepy_streamer_Brexit_aws.py

--- tweepy_streamer_Brexit_aws.py
+++ tweepy_streamer_Brexit_aws.py
@@ -75,15 +75,15 @@ c = conn.cursor()
 c.execute('''CREATE TABLE if NOT EXISTS users
              (number INT NOT NULL AUTO_INCREMENT,
              user_id CHAR(24) NOT NULL, user_screen_name CHAR(32), username CHAR(32),
-             user_description CHAR(512), user_location CHAR(100), user_followers INT, 
-             user_friends INT, user_created_at VARCHAR(200), PRIMARY KEY(user_id));''')
+             user_description TEXT(512), user_location CHAR(100), user_followers INT, 
+             user_friends INT, user_created_at VARCHAR(100), PRIMARY KEY(user_id));''')
 
 c.execute('''CREATE TABLE if NOT EXISTS twitter (
               id CHAR(32) NOT NULL,
               user_id CHAR(24),
               user_screen_name CHAR(32),
               username CHAR(32),
-              created_at CHAR(100),
+              created_at VARCHAR(100),
               full_text TEXT(512),
               in_reply_to_userid CHAR(24),
               in_reply_to_userscreename CHAR(32),
